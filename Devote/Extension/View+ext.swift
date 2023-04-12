c4e8d61b88c00ade57481051112415bd332d3762
Alex Vasyliev <olorin2007@gmail.com>
2023-04-12 21:14:08 +0200

    adjust UI

---
+++ Devote/Extension/View+ext.swift
@@ -0,0 +1,16 @@
+//
+//  View+ext.swift
+//  Devote
+//
+//  Created by Oleksii Vasyliev on 12.04.2023.
+//
+
+import SwiftUI
+
+#if canImport(UIKit)
+extension View {
+	func hideKeyboard() {
+		UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+	}
+}
+#endif
--- Devote/View/ContentView.swift
+++ Devote/View/ContentView.swift
@@ -10,72 +10,104 @@ import CoreData
 
 struct ContentView: View {
 	// MARK: - Properties
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
+	@State var task: String = ""
+	private var isButtonDisabled: Bool {
+		task.isEmpty
+	}
 	
-    private var items: FetchedResults<Item>
-
+	@Environment(\.managedObjectContext) private var viewContext
+	
+	@FetchRequest(
+		sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
+		animation: .default)
+	
+	private var items: FetchedResults<Item>
+	
 	// MARK: - Body
-    var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-						Text("Item at \(item.timestamp!, formatter: Constant.itemFormatter)")
-                    } label: {
-						Text(item.timestamp!, formatter: Constant.itemFormatter)
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
-        }
-    }
-
+	var body: some View {
+		NavigationView {
+			VStack {
+				VStack(spacing: 16) {
+					TextField("New Task", text: $task)
+						.padding()
+						.background(Color(UIColor.systemGray6))
+						.cornerRadius(10)
+					Button {
+						addItem()
+					} label: {
+						Spacer()
+						Text("SAVE")
+						Spacer()
+					}
+					.disabled(isButtonDisabled)
+					.padding()
+					.font(.headline)
+					.foregroundColor(.white)
+					.background(isButtonDisabled ? Color.gray : Color.pink)
+					.cornerRadius(10)
+				}
+				.padding()
+				List {
+					ForEach(items) { item in
+						VStack(alignment: .leading) {
+							Text(item.task ?? "")
+								.font(.headline)
+								.fontWeight(.bold)
+							Text("Item at \(item.timestamp!, formatter: Constant.itemFormatter)")
+								.font(.footnote)
+								.foregroundColor(.gray)
+							
+						}
+					}
+					.onDelete(perform: deleteItems)
+				}
+				.navigationTitle("Daily Tasks")
+				.toolbar {
+					ToolbarItem(placement: .navigationBarTrailing) {
+						EditButton()
+					}
+				}
+			}
+		}
+	}
+	
 	// MARK: - Methods
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
+	private func addItem() {
+		withAnimation {
+			let newItem = Item(context: viewContext)
+			newItem.timestamp = Date()
+			newItem.task = task
+			newItem.completion = false
+			newItem.id = UUID()
+			
+			do {
+				try viewContext.save()
+			} catch {
+				let nsError = error as NSError
+				fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+			}
+			
+			task = ""
+			hideKeyboard()
+		}
+	}
+	
+	private func deleteItems(offsets: IndexSet) {
+		withAnimation {
+			offsets.map { items[$0] }.forEach(viewContext.delete)
+			
+			do {
+				try viewContext.save()
+			} catch {
+				let nsError = error as NSError
+				fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+			}
+		}
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
-    }
+	static var previews: some View {
+		ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+	}
 }
